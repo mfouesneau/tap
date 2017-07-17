9c4c646c376b40ebe4bc027444292dc8b957afeb
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2017-07-17 12:05:40 +0200

    Packaging

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name = "tap",
+    version = 0.1,
+    description = "a simple table-access-protocol interface for python". 
+    long_description = readme(),
+    author = "Morgan Fouesneau",
+    author_email = "",
+    url = "https://github.com/mfouesneau/tap",
+    packages = find_packages(),
+    package_data = {},
+    include_package_data = True,
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Science/Research',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Topic :: Scientific/Engineering :: Astronomy'
+      ],
+    zip_safe=False
+)
